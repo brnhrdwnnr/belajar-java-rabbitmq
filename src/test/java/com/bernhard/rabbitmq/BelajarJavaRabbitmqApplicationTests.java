ee084fd3ab8cc44c51f7a6cb13c9c482e24e6db1
Bernhard Winner <bernhard@bernhardwinner@gmail.com>
2024-03-06 01:13:40 +0700

    feat(rabbit-mq):[PZN] belajar java rabbit mq

---
+++ src/test/java/com/bernhard/rabbitmq/BelajarJavaRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bernhard.rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarJavaRabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
